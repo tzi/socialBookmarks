c149058db544346a63bb8756e4809654088fa2c6
mlebrun <mlebrun@ALT015-M.local>
2012-12-10 14:25:21 +0100

    fixed indentation

--- social-bookmarks.js
+++ social-bookmarks.js
@@ -5,27 +5,27 @@
 
 ;(function(window, document){
 
-	var addEventListener = "addEventListener" in window
-		, eventListener = addEventListener ? "addEventListener" : "attachEvent"
-		, prefix = addEventListener ? "" : "on"
-		, clickEvent = "createTouch" in document ? "touchend" : "click"
+  var addEventListener = "addEventListener" in window
+    , eventListener = addEventListener ? "addEventListener" : "attachEvent"
+    , prefix = addEventListener ? "" : "on"
+    , clickEvent = "createTouch" in document ? "touchend" : "click"
 
-	function socialBookmarks(container){
-		var links = container.getElementsByTagName("a")
-			, length = links.length
-			, item
-		for(;length--;) {
-			item = links[length]
-			item[eventListener](prefix + clickEvent, function(event){
-				event = event || window.event 
-				if("preventDefault" in event) event.preventDefault()
-				else event.returnValue = false
+  function socialBookmarks(container){
+    var links = container.getElementsByTagName("a")
+      , length = links.length
+      , item
+    for(;length--;) {
+      item = links[length]
+      item[eventListener](prefix + clickEvent, function(event){
+        event = event || window.event 
+        if("preventDefault" in event) event.preventDefault()
+        else event.returnValue = false
 
-				window.open(item.href, "", "width=640, height=280")
-			})
-		}
-	}
+        window.open(item.href, "", "width=640, height=280")
+      })
+    }
+  }
 
-	document.socialBookmarks = socialBookmarks
+  document.socialBookmarks = socialBookmarks
 
 })(this, this.document)
